8312c9305e9a353624de7ad7b8a1784503a2c64f
benardop <benard.oc.p@gmail.com>
2024-08-21 05:03:54 -0300

    initial files commit

--- src/app/(pages)/page.tsx
+++ src/app/(pages)/page.tsx
@@ -1,6 +1,15 @@
+import { v2 as cloudinary } from 'cloudinary';
 import MediaGallery from '@/components/MediaGallery';
 
+cloudinary.config({
+  cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.NEXT_PUBLIC_CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+})
+
 export default async function Home() {
+  const results = await cloudinary.api.resources();
+  console.log('results', results);
   return (
     <div className="h-full mt-6">
       <MediaGallery
